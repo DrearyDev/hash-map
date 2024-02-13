1db7fcff78316f9da533421cc43a8378171c0feb
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-13 16:08:59 -0500

    Create hashSet factory function and export it

---
+++ hashSet.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import { linkedList } from "./linkedList.js";
+
+function hashSet() {
+    let table = new Array(16);
+    let capacity = 0;
+    let loadFactor = 0.75;
+
+    console.log('test');
+};
+
+export { hashSet };
